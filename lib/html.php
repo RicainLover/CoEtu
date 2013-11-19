904c8eb71eda748c30109a8c37ac6a7f91829b97
Killseto <bastien.doublet@orange.fr>
2013-11-19 09:02:18 +0100

    Update html.php

--- lib/html.php
+++ lib/html.php
@@ -10,26 +10,17 @@
 							print("<th>Prenom</th>");
 							print("<th>id de la zone</th>");
 							print("<th>prix de la place</th>");
-							print("<th>date representation</th>");		
-							print("<th>edition</th>");
-							print("<th>suppression</th>");
+							print("<th>date naissance</th>");		
+							print("<th>ville</th>");
+							print("<th>campus</th>");
 						print("</tr>");
 					print("</thead>");
-							foreach($liste as $element1 => $valeur){
-								print("<br/>");
-								if(is_array($valeur)){
-									print("<tr>");
-									$i = 2;
-									foreach($valeur as $element2 => $valeur2){
-											if($i%2 == 0){	
-												print("<td>".$valeur2."</td>");
-											}
-											$i = $i +1;
-										}
-									print("<td><a href=\"test_formulaire_table1.php?op=1&amp;id=".$valeur[0]."\"><img src=\"edit.png\" alt\"modif\" /> </a>");
-									print("<td><a href=\"test_vue_table1.php?op=2&amp;id=".$valeur[0]."\"><img src=\"delete.png\" alt\"suppr\" /> </a>");
-								}						
-							}							
+						foreach($liste as $element1 => $valeur){
+							print("<tr>");
+							print("<td>".$valeur2."</td>");
+						}
+								
+									
 	}
 
 ?>
