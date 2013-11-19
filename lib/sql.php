a1a2b5e79bd4cc90f819a2a526dfeacc1d9bf43d
Madahin <nico99999@live.fr>
2013-11-19 08:38:54 +0100

    Ajout de commentaire pour la vérification des connexion

--- lib/sql.php
+++ lib/sql.php
@@ -9,13 +9,18 @@ function verifID($pseudo,  $mdp)
         die($e->getMessage());
     }
     
+    // On considere le mot de passe comme juste
     $rep = true; 
     
+    // On recupere l'etudiant correspondant a l'identifiant fourni
     $select = $connec->query("SELECT * FROM ETUDIANT WHERE pseudo=$pseudo;");
+    // Si on trouve l'etudiant
     if ($select) {
+        // On verifie si le hash du mdp fourni et le meme que celui stocker
         if (hash("sha256", $mdp, null) != $select->mdp) {
             $rep = false;
         }
+    // Si on ne trouve pas l'etudiant
     }else{
         $rep = false;
     }
