a73af62900700170cf28971936ec5826173d174c
Hexatan <duvaux.gaetan@gmail.com>
2013-11-27 20:49:19 +0100

    Ajout page profil

---
+++ profil/index.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    require_once '../lib/securiter.php';
+    if(!isLogged()){
+        header("Location: ..");
+    }
+    require_once '../login.inc';
+    require_once '../lib/html.php';
+    require_once '../lib/sql.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Votre Profil</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<link rel='stylesheet' type='text/css' href='../css/inside.css' />
+		<script type='text/javascript' src='../js/inside.js' ></script>
+	</head>
+    <body>
+        <div id="titre">
+            <h1>Votre Profil</h1>
+            <span>Voyager n'a jamais été aussi simple</span>
+        </div>
+        <div id="cont"></div>
+        <?php boxuser(getNom($_SESSION["user_id"]),array("test3"=>"test4")); ?>
+        <?php nav(); ?>
+    </body>
+</html>
