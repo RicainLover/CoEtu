575b4d7f1396a682562c4790ddf9580d78f1c64e
Ricain <jeanmercadier@gmail.com>
2013-11-29 14:07:58 +0100

    rm comment

--- carnet/index.php
+++ carnet/index.php
@@ -23,25 +23,11 @@
         </div>
         <div id="carnet">
             <div id="contact">
+                <br />
+                <br />
+                <br />
+                <br />
                 <span class="label">Selectionner un contact pour l'afficher.</span>
-                <!-- <h2>Jean Mercadier</h2>
-                <span class="label">Univ:</span>
-                <span class="carac">IUT-BM</span>
-                <span class="label">Habite:</span>
-                <span class="carac">Lille</span>
-                <span class="label">Tél:</span>
-                <span class="carac">06 06 40 92 84</span>
-                <span class="label">Email:</span>
-                <a class="carac">jeanmercadier@gmail.com</a>
-                <span class="label">Skype:</span>
-                <span class="carac">jean.mercadier</span>
-                <span class="label">Facebook:</span>
-                <a class="carac">http://facebook.com/jean.mercadier</a>
-                <span class="label">Né:</span>
-                <span class="carac">Juil. 1993</span>
-                <div class="option">
-                    <a href="#">oublier</a>
-                </div> -->
             </div>
             <div id="liste">
 
