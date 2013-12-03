f4927148a9bd5e56b0711a438bba168051a084bb
Ricain <jeanmercadier@gmail.com>
2013-12-03 12:45:35 +0100

    securisation POST et GET

--- lib/securiter.php
+++ lib/securiter.php
@@ -1,6 +1,17 @@
 <?php
-function isLogged()
-{
+function isLogged() {
+	foreach ($_POST as $key => $value) {
+		$_POST[$key] = secureinput($value);
+	}
+	foreach ($_GET as $key => $value) {
+		$_GET[$key] = secureinput($value);
+	}
     return isset($_SESSION["user_id"]);
+}
+
+function secureinput($str){
+	$str = str_replace('\\','\\\\',$str);
+	$str = str_replace('\'', '\\\'', $str);
+	return $str;
 }
 ?>
